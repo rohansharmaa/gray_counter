141d228172bbd27e37c52ee1d1a1ec51bc7fa65f
Rohan Sharma <rohansharma573@gmail.com>
2020-05-16 21:35:44 +0530

    Update gray_counter.v

--- gray_counter.v
+++ gray_counter.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.10.2018 12:46:55
-// Design Name: 
-// Module Name: gray_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module gray_counter( clk,reset,count);
     input clk,reset;
